16d2ad2da987bc6e98dfd89724626270ab1bae32
ESMA SUYOLU <TCESUYOLU@tohas.local>
2021-05-02 14:39:52 +0300

    youtube angular course from Traversy Media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name: string = 'Esma Suyolu'
+
+  constructor() {
+    this.name = 'nazime Suyolu'
+    this.changeName('meryem karatas')
+  }
+
+  changeName(name: string): void {
+    this.name = name
+  }
 }
